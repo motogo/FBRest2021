c71e8cd702f176a96c00d6e70fb251d8141d56b5
motogo <horst.ender@softend.de>
2021-02-20 01:50:43 +0100

    GetSQLData verbessert

--- FBxRESTCore/config/version.go
+++ FBxRESTCore/config/version.go
@@ -2,6 +2,6 @@
 package config
 
 //Version Attributes of Application
-const AppName = "FBRest, REST Server for Firebird Database"
-const Version = "V2021.02.01_1"
+const AppName = "FBxRESTCore, REST Server for Firebird Database"
+const Version = "V21.02.17_1"
 const Copyright = "(c) SoftEnd, 2021"
--- FBxRESTCore/models/ModelData.go
+++ FBxRESTCore/models/ModelData.go
@@ -4,6 +4,9 @@ import (
 	
 	"database/sql"
 	"null"
+	"time"
+	"strconv"
+	"runtime"
 	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	_struct "fbrest/FBxRESTCore/struct"
@@ -20,10 +23,30 @@ type Db_init struct {
 }
 
 
+
+func bToKb(b uint64) uint64 {
+    return b / 1024  
+}
+
+func bToMb(b uint64) uint64 {
+    return b / 1024 / 1024 
+}
+
+func outMem(inf1,funcstr string) {
+	    var m runtime.MemStats
+	    runtime.ReadMemStats(&m)
+		log.Debug(" ")
+		//log.WithFields(log.Fields{inf1: m.Sys,	})        .Debug(funcstr+"->used total bytes system memory")
+	    log.WithFields(log.Fields{inf1: bToMb(m.Sys),	})        .Debug(funcstr+"->allocated total system memory")
+		log.WithFields(log.Fields{inf1: bToMb(m.TotalAlloc),	}).Debug(funcstr+"->used total heap memory       ")
+		log.WithFields(log.Fields{inf1: bToMb(m.Alloc),	})        .Debug(funcstr+"->allocated local heap memory  ")
+		log.WithFields(log.Fields{inf1: m.NumGC,	})            .Debug(funcstr+"->allocated GC memory          ")
+}
+
 func (model ModelGetData) GetSQLData(cmd string) (getStruct string, err error) {
 	const funcstr = "func models.GetSQLData"
-    log.WithFields(log.Fields{"Getting data": cmd,	}).Debug(funcstr)
-	
+    log.WithFields(log.Fields{"SQL command": cmd,	}).Debug(funcstr+"->start getting datas")
+	log.Debug(" ")
 	row, err := model.DB.Query(cmd)
 	if err != nil {				
 		log.WithFields(log.Fields{"Error": err.Error(),	}).Error(funcstr+"->query command")
@@ -31,84 +54,77 @@ func (model ModelGetData) GetSQLData(cmd string) (getStruct string, err error) {
 		return  "",err
 	} else {
 		
+		start := time.Now()		
+		runtime.GC()        
+        outMem("Memory before Query (Mb)",funcstr)
+		
 		colNames, err := row.Columns()
 		ct, err := row.ColumnTypes()
-		//dtt := ct[0].DatabaseTypeName()
-		//log.Debug(ct)
+
 		if err != nil {
-			log.WithFields(log.Fields{"Error": err.Error(),	}).Error(funcstr+"->get colnames")
+			log.WithFields(log.Fields{"Error": err.Error(),	}).Error(funcstr+"->getting colnames")
 		}
 		
 		readCols := make([]interface{}, len(colNames))
 		writeCols := make([]null.String, len(colNames))
-		var _isiStructAll [][] _struct.SqlResponseData
-		//var _isiStruct [] string
-
+		
 		for i, _ := range writeCols {
 			readCols[i] = &writeCols[i]
 		}
 		
+		var ergstr string = "["
+		var n int = 0
 		for row.Next() {
 			_isiStruct2 := make([]_struct.SqlResponseData, len(colNames))
 			err := row.Scan(readCols...)			
 			if err != nil {
 				log.WithFields(log.Fields{"Error": err.Error(),	}).Error(funcstr+"->scan next row")
 			} else {	
-				
-				for i, _ := range ct {
-					_isiStruct2[i].Colvalue  = writeCols[i].String
-					_isiStruct2[i].Colname   = ct[i].Name()
-					_isiStruct2[i].Datatype  = ct[i].DatabaseTypeName()										
-					_isiStruct2[i].IsNull    = !writeCols[i].Valid							
-				}
-
-				//log.Debug(_isiStruct2)
-				//pagesJson, err := json.Marshal(writeCols)
-				//if err != nil {
-				//	log.WithFields(log.Fields{"Error": err.Error(),	}).Error(funcstr+"->marshal to JSON")
-				//}
-				//_isiStruct = append(_isiStruct,string(pagesJson))
-				_isiStructAll = append(_isiStructAll,_isiStruct2)
-			}
-		
-	  	    
-		}
-		log.WithFields(log.Fields{"Getting data done": cmd,	}).Debug(funcstr)
-		    var erg [] string
-			var ergstr string = "["
-			for n, data := range _isiStructAll {
 				if(n == 0){
 					ergstr = ergstr + "{"
 				} else {
 					ergstr = ergstr + ",{"
 				}
+				n++
 				var sep string
-		        for i, _ := range data {
-					if(data[i].Datatype == "VARYING"){
+				for i, _ := range ct {
+					_isiStruct2[i].Colvalue  = writeCols[i].String
+					_isiStruct2[i].Colname   = ct[i].Name()
+					_isiStruct2[i].Datatype  = ct[i].DatabaseTypeName()										
+					_isiStruct2[i].IsNull    = !writeCols[i].Valid	
+					
+					
+					if(_isiStruct2[i].Datatype == "VARYING"){
 						sep = "\""
-				    } else if(data[i].Datatype == "DATE"){
+				    } else if(_isiStruct2[i].Datatype == "DATE"){
 						sep = "\""
-					} else if(data[i].Datatype == "TIMESTAMP"){
+					} else if(_isiStruct2[i].Datatype == "TIMESTAMP"){
 						sep = "\""
-					} else if(data[i].Datatype == "TIME"){
+					} else if(_isiStruct2[i].Datatype == "TIME"){
 						sep = "\""
 					} else {
 						sep = ""
 					}
 					if(i == 0){
-						ergstr = ergstr + "\""+data[i].Colname + "\":"+sep+data[i].Colvalue+sep	
+						ergstr = ergstr + "\""+_isiStruct2[i].Colname + "\":"+sep+_isiStruct2[i].Colvalue+sep	
 					} else {
-						ergstr = ergstr + ",\""+data[i].Colname + "\":"+sep+data[i].Colvalue+sep
+						ergstr = ergstr + ",\""+_isiStruct2[i].Colname + "\":"+sep+_isiStruct2[i].Colvalue+sep
 					}
-			        
-		        }
-				ergstr = ergstr + "}"
-				erg = append(erg,ergstr)
-		    }
-			ergstr = ergstr + "]"
-	    
+				}
+				ergstr = ergstr + "}"				
+			}						  	    
+		}
+		ergstr = ergstr + "]"
+		log.Debug(" ")
+		log.WithFields(log.Fields{"Numbers of rows ": strconv.Itoa(n) , })    .Debug(funcstr+"->got data complete       ")
+		
+		outMem("Memory after Query (Mb)",funcstr)
+		runtime.GC()
+		outMem("Memory after GC     (Mb)",funcstr)
+		log.Debug(" ")
+		elapsed := time.Since(start)
+		log.WithFields(log.Fields{"Time used  ": elapsed,	})            .Debug(funcstr+"->used time  ")
 		return ergstr, nil
-		//return _isiStruct, nil
 	}
 }
 
